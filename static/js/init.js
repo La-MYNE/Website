11039f6acb7f1640a74a4c24af71637b692308ea
Tim <timothee@indie.host>
2017-03-23 13:10:56 +0100

    add popup modal for news

--- static/js/init.js
+++ static/js/init.js
@@ -19,6 +19,20 @@ $('.grid').isotope({
 })
 
 
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('#modal1').modal('open');
+});
+
+$('#modal1').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '4%', // Starting top style attribute
+    endingTop: '10%', // Ending top style attribute
+  }
+);
 
 
 $(document).ready(function(){
