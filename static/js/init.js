a239324cb384d3261cca0bf5a34460472f6ce59e
unteem <timothee@unteem.org>
2017-11-14 14:20:03 +0100

    clean js

--- static/js/init.js
+++ static/js/init.js
@@ -29,11 +29,6 @@ $('.filters-button-group').on( 'click', 'button', function() {
 });
 
 
-
-
-
-
-
 $(document).ready(function(){
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('#modal1').modal('open');
@@ -50,117 +45,12 @@ $('#modal1').modal({
 );
 
 
-$(document).ready(function(){
-     $('.carousel').carousel({height: 600});
-   });
-
-
 $('.nav-pin').pushpin({ top:$('#beggining').height() });
 
 
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
   });
-
-
-
-  jQuery(function($){
-	'use strict';
-
-	// -------------------------------------------------------------
-	//   Basic Navigation
-	// -------------------------------------------------------------
-	(function () {
-		var $frame  = $('#basic');
-		var $slidee = $frame.children('ul').eq(0);
-		var $wrap   = $frame.parent();
-
-		// Call Sly on frame
-		$frame.sly({
-			horizontal: 1,
-			itemNav: 'basic',
-			smart: 1,
-			activateOn: 'click',
-			mouseDragging: 1,
-			touchDragging: 1,
-			releaseSwing: 1,
-			startAt: 3,
-			scrollBar: $wrap.find('.scrollbar'),
-			scrollBy: 1,
-			pagesBar: $wrap.find('.pages'),
-			activatePageOn: 'click',
-			speed: 300,
-			elasticBounds: 1,
-			easing: 'easeOutExpo',
-			dragHandle: 1,
-			dynamicHandle: 1,
-			clickBar: 1,
-
-			// Buttons
-			forward: $wrap.find('.forward'),
-			backward: $wrap.find('.backward'),
-			prev: $wrap.find('.prev'),
-			next: $wrap.find('.next'),
-			prevPage: $wrap.find('.prevPage'),
-			nextPage: $wrap.find('.nextPage')
-		});
-
-		// To Start button
-		$wrap.find('.toStart').on('click', function () {
-			var item = $(this).data('item');
-			// Animate a particular item to the start of the frame.
-			// If no item is provided, the whole content will be animated.
-			$frame.sly('toStart', item);
-		});
-
-		// To Center button
-		$wrap.find('.toCenter').on('click', function () {
-			var item = $(this).data('item');
-			// Animate a particular item to the center of the frame.
-			// If no item is provided, the whole content will be animated.
-			$frame.sly('toCenter', item);
-		});
-
-		// To End button
-		$wrap.find('.toEnd').on('click', function () {
-			var item = $(this).data('item');
-			// Animate a particular item to the end of the frame.
-			// If no item is provided, the whole content will be animated.
-			$frame.sly('toEnd', item);
-		});
-
-		// Add item
-		$wrap.find('.add').on('click', function () {
-			$frame.sly('add', '<li>' + $slidee.children().length + '</li>');
-		});
-
-		// Remove item
-		$wrap.find('.remove').on('click', function () {
-			$frame.sly('remove', -1);
-		});
-	}());
-});
-
-
-
-
-$(document).ready(function(){
-  $(".responsive").slick({
-    autoplay: true,
-    dots: true,
-    responsive: [{
-        breakpoint: 500,
-        settings: {
-            dots: false,
-            arrows: false,
-            infinite: false,
-            slidesToShow: 2,
-            slidesToScroll: 2
-        }
-    }]
-});
-
-});
 
 $(document).ready(function(){
   $('ul.tabs').tabs();
