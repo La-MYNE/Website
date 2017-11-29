0074d714467dd289cddac26d219015f30fc34b16
unteem <timothee@unteem.org>
2017-11-29 14:35:26 +0100

    filter buttons

--- static/js/init.js
+++ static/js/init.js
@@ -22,11 +22,41 @@ var $grid = $('.grid').isotope({
 
 // init Isotope
 
+
+// store filter for each group
+var filters = {};
+
 // bind filter button click
 $('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
+  var $this = $(this);
+  // get group key
+  var $buttonGroup = $this.parents('.button-group');
+  var filterGroup = $buttonGroup.attr('data-filter-group');
+  // set filter for group
+  filters[ filterGroup ] = $this.attr('data-filter');
+  // combine filters
+  var filterValue = concatValues( filters );
   $grid.isotope({ filter: filterValue });
 });
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
 
 
 $(document).ready(function(){
