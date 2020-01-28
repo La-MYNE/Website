1f73703232c8b3edcb6d1cfc183e497aaed53a87
Timothee Gosselin <timothee@indie.host>
2020-01-28 14:25:23 +0100

    add isotope filters to agenda

--- static/js/init.js
+++ static/js/init.js
@@ -1,15 +1,9 @@
-var options = {
-  valueNames: [ 'name', 'born' ]
-};
-
-var values = [ {
-  name: "Martina Elm",
-  born: 1986
-}];
-
-var userList = new List('users', options, values);
-
-userList.add({
-  name: "Gustaf Lindqvist",
-  born: 1983
+// init Isotope
+var $grid = $('.grid').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
 });
